30903fd0ca2972b8556d4912109426f36e1a7cc8
sarraj23 <78157490+sarraj23@users.noreply.github.com>
2021-02-01 21:48:00 +0300

    yes, jumping is a hobby

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "Saqer"
+let age = "4"
+let hobby = "jumping"
+
+console.log(age=4)
+
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you will have years of ${hobby} practice`)
